cae56e6f9052fa9b4a886ba9b4b22a20d9b65cbd
DaiND <daind@sudobo.com>
2021-06-04 06:52:07 +0700

    edit structure product list in category product screen

--- wordpress/wp-content/themes/moc-le-gia/template-parts/category-product.php
+++ wordpress/wp-content/themes/moc-le-gia/template-parts/category-product.php
@@ -1,4 +1,4 @@
-<div class="col-xs-12 col-sm-6 col-md-3">
+<div class="col-xs-12 col-sm-6 col-md-4">
     <div class="category__product-item">
         <figure class="featured-thumbnail thumbnail ">
             <a href="<?php echo esc_url(get_permalink()) ?>" title="<?php echo get_the_title(); ?>">
